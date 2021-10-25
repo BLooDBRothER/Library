27ebf473bac8878accb5cbb2bb06a650804f20f4
BLooDBRothER <computertechnology446@gmail.com>
2021-10-25 13:20:41 +0530

    bug fix

--- js/main.js
+++ js/main.js
@@ -5,19 +5,22 @@ const addBookForm = document.querySelector(".add-book-form");
 const closeFormIC = document.querySelectorAll(".close-form");
 
 addBookIC.addEventListener("click", (e) => {
+    document.querySelector("body").style.overflowY = "hidden";
+    formCnt.style.top = document.querySelector("html").scrollTop + "px";
     toggleDisplay([formCnt, addBookForm], [1, 1]);
 });
 
 closeFormIC.forEach(IC => {
     IC.addEventListener("click", (e) => {
+        document.querySelector("body").style.overflowY = "initial"
         toggleDisplay([formCnt, addBookForm], [0, 0]);
     });
 });
 
 //window listener
 window.addEventListener("keyup", (e) => {
-    console.log(e);
     if(e.key === "Escape" && !formCnt.classList.contains("none")){
+        document.querySelector("body").style.overflowY = "initial"
         toggleDisplay([formCnt, addBookForm], [0, 0]);
     }
 });

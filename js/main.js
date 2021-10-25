b9f4a827be4c895fac75aaa3859b7c266f38b27a
BLooDBRothER <computertechnology446@gmail.com>
2021-10-25 13:09:12 +0530

    Add toggle form functionality for add books

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Add book - form
+const addBookIC = document.querySelector(".control-ic[data-icon='books']");
+const formCnt = document.querySelector(".add-forms");
+const addBookForm = document.querySelector(".add-book-form");
+const closeFormIC = document.querySelectorAll(".close-form");
+
+addBookIC.addEventListener("click", (e) => {
+    toggleDisplay([formCnt, addBookForm], [1, 1]);
+});
+
+closeFormIC.forEach(IC => {
+    IC.addEventListener("click", (e) => {
+        toggleDisplay([formCnt, addBookForm], [0, 0]);
+    });
+});
+
+//window listener
+window.addEventListener("keyup", (e) => {
+    console.log(e);
+    if(e.key === "Escape" && !formCnt.classList.contains("none")){
+        toggleDisplay([formCnt, addBookForm], [0, 0]);
+    }
+});
+
+// function to toggle display none
+function toggleDisplay([...elem], [...value]){
+    elem.forEach((each, idx) => {
+        value[idx] ? each.classList.remove("none") : each.classList.add("none");
+    });
+}

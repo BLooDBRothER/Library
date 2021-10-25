5e22dc40002fb96a88a31c7208d77435569eb603
BLooDBRothER <computertechnology446@gmail.com>
2021-10-25 16:37:17 +0530

    Add toggle collections function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,63 @@
-// Add book - form
+import { bookCardTemplate } from "./template.js";
+
+// Library Class Logic
+class Book{
+    constructor(title, author, completedPages, totalPages, isRead=false, groups=[]){
+        this.title = title;
+        this.author = author,
+        this.completedPages = completedPages,
+        this.totalPages = totalPages,
+        this.isRead = isRead,
+        this.groups = groups
+    }
+}
+
+class library{
+    constructor(books=[], libraryGroups=[]){
+        this.books = books;
+        this.libraryGroups = libraryGroups;
+    }
+
+    addBook(book){
+        this.books.push(book);
+    }
+
+    displayBook(books){
+        books.forEach(book => {
+            bookCardsCnt.appendChild(bookCardTemplate(book.title, book.author, book.completedPages, book.totalPages, book.isRead, book.groups));
+        });
+    }
+}
+
+// DOM
 const addBookIC = document.querySelector(".control-ic[data-icon='books']");
+const addCollectionIC = document.querySelector(".control-ic[data-icon='collections']");
+const toggleCollectionIC = document.querySelector(".control-ic[data-icon='toggle-collections']");
+const librarySearch = document.querySelector(".library-search"); 
+const sectionTitle = document.querySelector(".section-title");
+const bookCardsCnt = document.querySelector(".book-cards");
+const bookCollectionsCnt = document.querySelector(".book-collections");
 const formCnt = document.querySelector(".add-forms");
 const addBookForm = document.querySelector(".add-book-form");
-const closeFormIC = document.querySelectorAll(".close-form");
+const closeFormIC = document.querySelector(".close-form");
+
+toggleCollectionIC.addEventListener("click", function(e){
+    if(this.dataset.toggled === "false"){
+        this.style.fill = "var(--secondary-color)";
+        librarySearch.placeholder = "Search Collections";
+        this.dataset.toggled = "true";
+        sectionTitle.innerText = "Collections";
+        toggleDisplay([addBookIC, addCollectionIC, bookCardsCnt, bookCollectionsCnt], [0, 1, 0 ,1]);
+    }
+    else{
+        this.style.fill = "var(--form)";
+        this.dataset.toggled = "false";
+        librarySearch.placeholder = "Search Books";
+        sectionTitle.innerText = "All Books";
+        toggleDisplay([addBookIC, addCollectionIC, bookCardsCnt, bookCollectionsCnt], [1, 0, 1 ,0]);
+
+    }
+});
 
 addBookIC.addEventListener("click", (e) => {
     document.querySelector("body").style.overflowY = "hidden";
@@ -10,11 +65,9 @@ addBookIC.addEventListener("click", (e) => {
     toggleDisplay([formCnt, addBookForm], [1, 1]);
 });
 
-closeFormIC.forEach(IC => {
-    IC.addEventListener("click", (e) => {
-        document.querySelector("body").style.overflowY = "initial"
-        toggleDisplay([formCnt, addBookForm], [0, 0]);
-    });
+closeFormIC.addEventListener("click", (e) => {
+    document.querySelector("body").style.overflowY = "initial"
+    toggleDisplay([formCnt, addBookForm], [0, 0]);
 });
 
 //window listener

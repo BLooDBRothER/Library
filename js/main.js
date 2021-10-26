5204f3dd6aa7309f11d427d8836ad6c1b63a5620
BLooDBRothER <computertechnology446@gmail.com>
2021-10-26 13:40:28 +0530

    complete add and edit book

--- js/main.js
+++ js/main.js
@@ -1,46 +1,44 @@
 import { bookCardTemplate } from "./template.js";
 
-// Library Class Logic
-class Book{
-    constructor(title, author, completedPages, totalPages, isRead=false, groups=[]){
-        this.title = title;
-        this.author = author,
-        this.completedPages = completedPages,
-        this.totalPages = totalPages,
-        this.isRead = isRead,
-        this.groups = groups
-    }
+// Library Object Prototype Logic
+
+function Book(title, author, completedPages, totalPages, isRead=false, groups=[]){
+    this.title = title;
+    this.author = author,
+    this.completedPages = completedPages,
+    this.totalPages = totalPages,
+    this.isRead = isRead,
+    this.groups = groups
 }
 
-class Library{
-    constructor(books=[], libraryGroups=[]){
-        this.books = books;
-        this.libraryGroups = libraryGroups;
-    }
-
-    addBook(book){
-        this.books.push(book);
-        this.displayBook(this.books[this.books.length-1]);
-    }
-
-    checkBookExists(newBook){
-        return this.books.find(book => book.title === newBook.title);
-    }
-
-    displayBook(book){
-        bookCardsCnt.appendChild(bookCardTemplate(book.title, book.author, book.completedPages, book.totalPages, book.isRead, book.groups));
-    }
-
-    displayAllBooks(books){
-        books.forEach(book => {
-            this.displayBook(book);
-        });
-    }
+function Library(books=[], libraryGroups=[]){
+    this.books = books;
+    this.libraryGroups = libraryGroups;
 }
 
-// DOM
+Library.prototype.addBook = function (book){
+    this.books.push(book);
+}
+
+Library.prototype.updateBook = function (idx, book){
+    this.books[idx] = book;
+}
+
+Library.prototype.removeBook = function (title){
+    this.books = this.books.filter(book => book.title !== title);
+}
+
+Library.prototype.returnBook = function (title){
+    return this.books.findIndex(book => book.title === title);
+}
+
+Library.prototype.checkBookExists = function (newBook){
+    return this.books.some(book => book.title === newBook.title);
+}
+
 const myLibrary = new Library();
-
+// DOM
+console.log(myLibrary)
 const addBookIC = document.querySelector(".control-ic[data-icon='books']");
 const addCollectionIC = document.querySelector(".control-ic[data-icon='collections']");
 const toggleCollectionIC = document.querySelector(".control-ic[data-icon='toggle-collections']");
@@ -61,6 +59,13 @@ const error = addBookForm.querySelectorAll(".err");
 const totalPages = addBookForm.querySelector(".book__form-text[data-name='totPage']");
 const isReadCheckBox = addBookForm.querySelector(".book__form-checkbox");
 
+function toggleBookForm(){
+    document.querySelector("body").style.overflowY = "hidden";
+    formCnt.style.top = document.querySelector("html").scrollTop + "px";
+    toggleDisplay([formCnt, addBookForm], [1, 1]);
+    addBookForm.querySelectorAll(".book__form-text").forEach(elem => {elem.value = "";})
+}
+
 toggleCollectionIC.addEventListener("click", function(e){
     if(this.dataset.toggled === "false"){
         this.style.fill = "var(--secondary-color)";
@@ -79,12 +84,7 @@ toggleCollectionIC.addEventListener("click", function(e){
     }
 });
 
-addBookIC.addEventListener("click", (e) => {
-    document.querySelector("body").style.overflowY = "hidden";
-    formCnt.style.top = document.querySelector("html").scrollTop + "px";
-    toggleDisplay([formCnt, addBookForm], [1, 1]);
-    addBookForm.querySelectorAll(".book__form-text").forEach(elem => {elem.value = "";})
-});
+addBookIC.addEventListener("click", toggleBookForm);
 
 closeFormIC.addEventListener("click", (e) => {
     document.querySelector("body").style.overflowY = "initial"
@@ -125,8 +125,12 @@ totalPages.addEventListener("input", function(e){
 
 formBtn.forEach(btn => {
     btn.addEventListener("click", function(e){
-        if(this.dataset.value === "book"){
+        if(this.dataset.value === "book" && this.innerText === "Add To Library"){
             addBook();
+        }
+        else if(this.dataset.value === "book" && this.innerText === "Update Book"){
+            this.innerText = "Add To Library";
+            updateBook();
         }
     });
 });
@@ -143,9 +147,19 @@ function checkFilled(){
     });
     return returnValue;
 }
+
+function createBook(){
+    const bookTitle = replaceAngularBracket(title.value);
+    const bookAuthor = replaceAngularBracket(author.value);
+    const bookCompletedPages = +completedPages.value;
+    const bookTotalPages = +totalPages.value;
+    const bookIsRead = isReadCheckBox.checked;
+    return new Book(bookTitle, bookAuthor, bookCompletedPages, bookTotalPages, bookIsRead);
+}
+
 function addBook(){
     if(checkFilled()) return;
-    const newBook = new Book(title.value, author.value, +completedPages.value, +totalPages.value, isReadCheckBox.checked);
+    const newBook = createBook();
     if(myLibrary.checkBookExists(newBook)) {
         error[0].innerText = "Book Already exists";
         title.style.borderColor = "var(--negative)";
@@ -153,7 +167,52 @@ function addBook(){
         return
     };
     myLibrary.addBook(newBook);
+    displayBook(newBook);
     closeFormIC.click();
+}
+
+function updateBookForm(bookTitle){
+    const bookFormBtn = document.querySelector(".form_button[data-value='book']");
+    bookFormBtn.innerText = "Update Book";
+    toggleBookForm();
+    const bookIdx = myLibrary.returnBook(bookTitle);
+    bookFormBtn.dataset.idx = bookIdx;
+    const book = myLibrary.books[bookIdx];
+    title.value = book.title;
+    author.value = book.author;
+    completedPages.value = book.completedPages;
+    totalPages.value = book.totalPages;
+    isReadCheckBox.checked = book.isRead;
+}
+
+function updateBook(){
+
+}
+
+function editBook(e){
+    const bookTitle = e.target.parentElement.dataset.title;
+    console.log(bookTitle);
+    if(e.target.dataset.value === "delete"){
+        myLibrary.removeBook(bookTitle);
+        console.log(bookCardsCnt);
+        bookCardsCnt.removeChild(bookCardsCnt.querySelector(`.book-card[data-title="${bookTitle}"]`));
+    }
+    if(e.target.dataset.value === "edit"){
+        updateBookForm(bookTitle);
+    }
+}
+
+function displayBook(book){
+    bookCardsCnt.appendChild(bookCardTemplate(book.title, book.author, book.completedPages, book.totalPages, book.isRead));
+    bookCardsCnt.lastChild.querySelectorAll(".book-options-ic").forEach(option => {
+        option.addEventListener("click", editBook);
+    });
+}
+
+function displayAllBooks(books){
+    books.forEach(book => {
+        this.displayBook(book);
+    });
 }
 
 //window listener
@@ -164,9 +223,18 @@ window.addEventListener("keyup", (e) => {
     }
 });
 
+// util function
+
 // function to toggle display none
 function toggleDisplay([...elem], [...value]){
     elem.forEach((each, idx) => {
         value[idx] ? each.classList.remove("none") : each.classList.add("none");
     });
+}
+
+// function to replace angular brackets
+function replaceAngularBracket(string = ""){
+    string = string.replace(/</gi, "&lt;");
+    string = string.replace(/>/gi, "&gt;");
+    return string;
 }
